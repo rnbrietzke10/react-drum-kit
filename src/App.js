55d4f3b606c4c603b8f07407f6571997d8fd4d1c
Rebecca Brietzke <rebeccabrietzke@Rebeccas-MacBook-Pro.local>
2021-08-28 11:12:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React from 'react';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 
+// const themes = {
+// 	light: light Theme,
+// };
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Drumkit>
+				<Display> Text </Display>
+				<KeyContainer>
+					<KeyRow>
+						<Key>Q</Key>
+						<Key>W</Key>
+						<Key>E</Key>
+					</KeyRow>
+					<KeyRow>
+						<Key>A</Key>
+						<Key>S</Key>
+						<Key>D</Key>
+					</KeyRow>
+					<KeyRow>
+						<Key>Z</Key>
+						<Key>X</Key>
+						<Key>C</Key>
+					</KeyRow>
+				</KeyContainer>
+			</Drumkit>
+		</Wrapper>
+	);
 }
 
 export default App;
+
+const Wrapper = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: lightblue;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Drumkit = styled.div`
+	width: 400px;
+	height: 400px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Display = styled.div`
+	background-color: red;
+	height: 100px;
+	width: 350px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const KeyContainer = styled.div`
+	background-color: green;
+	height: 275px;
+	width: 350px;
+	display: flex;
+	flex-direction: column;
+`;
+
+const KeyRow = styled.div`
+	height: 33%;
+	width: 100%;
+	display: flex;
+	background-color: blue;
+`;
+
+const Key = styled.div`
+	width: 33%;
+	background-color: ghostwhite;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
